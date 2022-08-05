e2a020b60ad4c13de7502afdc5d0ce165a9f4aa0
RizkiDwiN <dwirizki0510@gmail.com>
2022-08-05 20:06:16 +0700

    First Commit

---
+++ app/src/main/java/com/rizki/livetrackingmaps/MapsActivity.java
@@ -0,0 +1,132 @@
+package com.rizki.livetrackingmaps;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rizki.livetrackingmaps.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    private DatabaseReference reference;
+    private LocationManager manager;
+
+    private final int MIN_TIME = 1000; // 1 detik
+    private final int MIN_DISTANCE = 1; // 1 meter
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        reference = FirebaseDatabase.getInstance().getReference().child("User-101");
+        //FirebaseDatabase.getInstance().getReference().setValue("This is tracker app");
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        getLocationUpdates();
+    }
+
+    private void getLocationUpdates() {
+        if(manager != null) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                if (manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
+                } else if (manager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+                    manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
+                } else {
+                    Toast.makeText(this, "No Provider Enabled", Toast.LENGTH_SHORT).show();
+                }
+
+            }else{
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            }
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResult){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResult);
+            if(requestCode == 101){
+                if(grantResult.length > 0 && grantResult[0] == PackageManager.PERMISSION_GRANTED){
+                    getLocationUpdates();
+                }else{
+                    Toast.makeText(this, "Permission Required", Toast.LENGTH_SHORT).show();
+                }
+            }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-6.839608884201539, 107.6052857179903);
+        googleMap.setMapType(googleMap.MAP_TYPE_HYBRID);
+        MarkerOptions options = new MarkerOptions().position(sydney).title("Lokasi Saat Ini");
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney,17));
+        googleMap.addMarker(options);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(location != null){
+            saveLocation(location);
+        }else{
+            Toast.makeText(this, "No Location", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void saveLocation(Location location) {
+        reference.setValue(location);
+    }
+
+    @Override
+    public void onStatusChanged(String provider,int status, Bundle extras){
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider){
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider){
+
+    }
+}
